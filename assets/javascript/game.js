a8217f0be7f8feaa758a6d21876b5841db3ce36c
Corey Walsh <matthias61984@hotmail.com>
2018-08-25 16:01:27 -0700

    incomplete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,66 +18,70 @@ var randomize = Math.floor(Math.random()*wordBank.length);
 var wordToGuess = wordBank[randomize];
 // Set empty array that will contain the current word
 var wordToGuessArr = [];
-// Defines empty array that will hold incorrectly guessed letters
-var guessedLetters = [];
-// Set open array for positions
-var positions = [];
+// Defines empty array that will hold wrong letters
+var wrongLetters = [];
 // Set guesses left to zero
 var guessesLeft = 10;
-// Set global letter variable
-var letter = "";
 // Set win counter
 var wins = 0;
 // Set loss counter
 var losses = 0;
-// Target word-blanks h3 element and store in variable
+// Target word-blanks element and store in variable
 var targetWordBlanks = document.getElementById("word-blanks");
+// Target guesses-left element and store in variable
+var targetGuessesLeft = document.getElementById("guesses-left");
+// Target wrong-guesses element and store in variable
+var targetWrongGuesses = document.getElementById("wrong-guesses");
+// Target loss-counter element and store in variable
+var targetLossCounter = document.getElementById("loss-counter");
 // DEFINE ALL FUNCTIONS
 // Define function to set up the game: choose and present hidden word
 function startGame() {
     for (var i=0; i < wordToGuess.length; i++) {
         wordToGuessArr.push("_");
-    }
+    };
     var str = wordToGuessArr.join(" ");
     targetWordBlanks.innerHTML = str;
+    console.log(wordToGuess);
 };
-// Play game: if/else deciding if guess = letter in wordToGuess
+// When aletter key is pressed
 startGame();
 document.onkeyup = makeGuess;
 function makeGuess() {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if (event.keyCode >= 65 && event.keyCode <= 90 ) {
         var eventLetter = String.fromCharCode(event.keyCode);
         var letter = eventLetter.toLowerCase();
         checkGuess(letter);
     };
 };
 
+
+// What to do when letter is wrong
+function wrongGuess(letter) {
+    wrongLetters.push(letter);
+    guessesLeft == guessesLeft--;
+    targetWrongGuesses.innerHTML = wrongLetters;
+    targetGuessesLeft.innerHTML = guessesLeft;
+    if (guessesLeft < 0) {
+        alert("You burned it!");
+        losses == losses++;
+        targetLossCounter.innerHTML = losses;
+        startGame();
+    };
+}
+// Check the letter to se if it's in the word
 function checkGuess(letter) {
-// Loop through word finding all instances of guessed letter, store the indicies in an array.
-    for (var i = 0; i < wordToGuess.length; i++) {
-        if(wordToGuess[i] === letter) {
-            positions.push(i);
-            console.log(positions[i]);
+    if (wrongLetters.includes(letter) == true) {
+        alert("You already guessed that letter, you donkey!");
+    } else if (wordToGuess.includes(letter) == true) {
+        for (var i = 0; i < wordToGuess.length; i++) {
+            if (wordToGuess[i] === letter) {
+                wordToGuessArr[i] = letter;
+                var displayLetter = wordToGuessArr.join(" ");
+                targetWordBlanks.innerHTML = displayLetter;
+            };
         };
-    };
-// if there are no indicies, remove a guess and update the hangman image
-    if (positions.length > 0) {
-        correctGuess();
-    } else {
-        incorrectGuess();
-    };
+    } else if (wordToGuess.includes(letter) == false) {
+        wrongGuess(letter);
+    }
 };
-
-function correctGuess() {
-    for(var i = 0; i < positions.length; i++) {
-        wordToGuess[positions[i]] = letter;
-        console.log(letter);
-    };
-};
-
-function incorrectGuess() {
-    guessesLeft = guessesLeft--;
-    console.log("Guesses left: " + guessesLeft);
-};
-
-// // Finish game: reset game and counter up wins or losses when word is finished or tries run out
