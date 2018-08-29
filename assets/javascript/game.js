0d5d7aa96f6f7160c227d7e5b8162c17875b0fef
Corey Walsh <matthias61984@hotmail.com>
2018-08-28 18:44:13 -0700

    needs win function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,9 @@ var wordToGuessArr = [];
 // Defines empty array that will hold wrong letters
 var wrongLetters = [];
 // Set guesses left to zero
-var guessesLeft = 10;
+var guessesLeft = 8;
+// Define empty array to hold win check
+var winArray = [];
 // Set win counter
 var wins = 0;
 // Set loss counter
@@ -34,6 +36,8 @@ var targetGuessesLeft = document.getElementById("guesses-left");
 var targetWrongGuesses = document.getElementById("wrong-guesses");
 // Target loss-counter element and store in variable
 var targetLossCounter = document.getElementById("loss-counter");
+// Target win-counter element and store in variable
+var targetWinCounter = document.getElementById("win-counter");
 // DEFINE ALL FUNCTIONS
 // Define function to set up the game: choose and present hidden word
 function startGame() {
@@ -44,7 +48,7 @@ function startGame() {
     targetWordBlanks.innerHTML = str;
     console.log(wordToGuess);
 };
-// When aletter key is pressed
+// When a letter key is pressed
 startGame();
 document.onkeyup = makeGuess;
 function makeGuess() {
@@ -54,7 +58,6 @@ function makeGuess() {
         checkGuess(letter);
     };
 };
-
 
 // What to do when letter is wrong
 function wrongGuess(letter) {
@@ -66,12 +69,19 @@ function wrongGuess(letter) {
         alert("You burned it!");
         losses == losses++;
         targetLossCounter.innerHTML = losses;
+        wordToGuessArr = [];
+        wrongLetters = [];
+        targetWrongGuesses.innerHTML = wrongLetters;
+        guessesLeft = 8;
+        targetGuessesLeft.innerHTML = guessesLeft;
+        var wordToGuess = wordBank[randomize];
         startGame();
     };
-}
+};
+
 // Check the letter to se if it's in the word
 function checkGuess(letter) {
-    if (wrongLetters.includes(letter) == true) {
+    if (wrongLetters.includes(letter) == true || wordToGuessArr.includes(letter) == true) {
         alert("You already guessed that letter, you donkey!");
     } else if (wordToGuess.includes(letter) == true) {
         for (var i = 0; i < wordToGuess.length; i++) {
@@ -79,9 +89,28 @@ function checkGuess(letter) {
                 wordToGuessArr[i] = letter;
                 var displayLetter = wordToGuessArr.join(" ");
                 targetWordBlanks.innerHTML = displayLetter;
+                winArray.push(letter);
             };
         };
     } else if (wordToGuess.includes(letter) == false) {
         wrongGuess(letter);
-    }
+    };
+    checkWin();
+};
+
+// Check for win conditions and reset the game
+function checkWin() {
+    if (winArray.length == wordToGuess.length) {
+        alert("Fantastic, you CAN cook!");
+        wins == wins++;
+        targetWinCounter.innerHTML = wins;
+        wordToGuessArr = [];
+        wrongLetters = [];
+        winArray = [];
+        targetWrongGuesses.innerHTML = wrongLetters;
+        guessesLeft = 8;
+        targetGuessesLeft.innerHTML = guessesLeft;
+        var wordToGuess = wordBank[randomize];
+        startGame();
+    };
 };
